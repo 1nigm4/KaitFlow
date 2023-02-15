05c7ca29ff77d216efb3b57e4a851d684a441341
Student <Student@JUNIOR-TMP-02>
2023-02-15 15:36:35 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Flow
+{
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
+    }
+}
