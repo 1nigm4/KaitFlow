4fe8662bebc83b65ad7aac9d64002630d2b6a4b4
1nigm4 <geloyzer@gmail.com>
2023-02-22 16:01:36 +0300

    Create options window

---
+++ Windows/OptionsWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+using System.Windows;
+
+namespace Flow.Windows
+{
+    public partial class OptionsWindow : Window
+    {
+        public OptionsWindow()
+        {
+            InitializeComponent();
+            StreamUrl.Text = ConfigurationManager.AppSettings.Get("StreamUrl");
+            BackgroundUrl.Text = ConfigurationManager.AppSettings.Get("BackgroundUrl");
+        }
+
+        private void BackgroundUrl_DragOver(object sender, DragEventArgs e) => e.Handled = true;
+        private void BackgroundUrl_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                BackgroundUrl.Text = files[0];
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string streamUrl = StreamUrl.Text;
+            string backgroundUrl = BackgroundUrl.Text;
+            if (string.IsNullOrWhiteSpace(streamUrl) || string.IsNullOrWhiteSpace(backgroundUrl)) return;
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["StreamUrl"].Value = streamUrl;
+            config.AppSettings.Settings["BackgroundUrl"].Value = backgroundUrl;
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e) => this.Close();
+    }
+}
